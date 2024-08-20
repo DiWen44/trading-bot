63f2611a0772e0193d083e9278d9e7187f0f6779
Devin Sidhu <devinsidhu24@gmail.com>
2024-08-20 18:01:01 +0100

    Started work on sentiment analysis

--- src/bot.py
+++ src/bot.py
@@ -2,12 +2,18 @@ from dotenv import load_dotenv
 import os
 import requests
 import csv
+import json
+import re
+from datetime import datetime
 
 import pandas as pd
 import numpy as np
 
 import finnhub
 
+import nltk
+nltk.download() # Download NLTK data
+from nltk.corpus import stopwords
 
 
 class Bot():
@@ -19,6 +25,7 @@ class Bot():
 		newsapi_key - The API key for newsAPI
 		watchlist - An array of 2-value tuples each in form (name, symbol). These are the stocks we will be trading. Loaded from watchlist.csv
 	"""
+
 
 	def __init__(self):
 
@@ -36,18 +43,81 @@ class Bot():
 			self.watchlist = [row for row in reader]
 
 
-
 	def get_news(self, days, company):
 		"""
-		Gets an array of news headlines from newsAPI
+		Gets current date's news articles from newsAPI for a given company.
+		Returned in the form of an array of strings, each string representing the content of an article
 
 		PARAMS:
 			days - Number of days prior to get news from (e.g. days=3 will get headlines from the last 3 days)
 			company - Name of the company to get news for
 		"""
 
-		# Get a JSON object of the news
-		news = requests.get(f"https://newsapi.org/v2/everything?q={company}&from=2024-08-20&sortBy=popularity&apiKey={self.newsapi_key}").json()
+		# Get a dictionary object of the news (newsAPI returns a JSON object)
+		date = datetime.today().strftime('%Y-%m-%d')
+		newsJSON = requests.get(f"https://newsapi.org/v2/everything?q={company}&from={date}&sortBy=popularity&apiKey={self.newsapi_key}").json()
+		news = json.loads(newsJSON)
+
+		articles = [ article['content'] for article in news['articles'] ]
+		return articles
+
+
+
+	def clean_article(self, text)
+		"""
+		Given the content of an article, "clean" it to prepare for sentiment analysis by:
+			- Removing non-letter chars
+			- Tokenizing
+			- Setting to lowercase
+			- Removing stopwords
+
+		returns a tokenized (i.e. array of words) and cleaned version of the original text
+
+		PARAMS:
+			Text - the content of the article
+		"""
+
+		letters_only = re.sub("[^a-zA-Z]"," ", text) # Remove non-letters 
+		tokens = letters_only.lower().split() # Lowercase & tokenize
+
+		# Remove stopwords
+		tokens = [tok for tok in tokens if not tok in stopwords.words('english')]
+
+		return tokens
+
+
+	def est_sentiment(self, company):
+		"""
+		Returns the estimated sentiment for a given company, based on news found by self.get_news()
+		The resulting sentiment is based on the titles and descriptions of news articles
+		"""
+
+		articles = self.get_news(3, company)
+
+		for article in articles:
+			article_tokens = self.clean_article(article)
+
+			# Create bag_of_words
+			bag_of_words
+			for word in article:
+				try:
+					bag_of_words[word] += 1
+				except KeyError:
+					bag_of_words[word] = 1
+
+
+
+
+
+
+
+
+
+
+			
+
+
+
 
 
 
