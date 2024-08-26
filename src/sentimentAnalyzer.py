1d8ddcdeec8ec57603ef1f9ce41918d2e6076ef7
Devin Sidhu <devinsidhu24@gmail.com>
2024-08-26 14:39:43 +0100

    Created average_feature_vec method to allow us to get a single vector representing an entire headline

--- src/sentimentAnalyzer.py
+++ src/sentimentAnalyzer.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 
 from sklearn.ensemble import RandomForestClassifier
 from gensim.models import word2vec
@@ -13,11 +14,12 @@ class SentimentAnalyzer():
 	Represents the bot's sentiment analysis engine
 
 	The sentiment analysis engine consists of 1 parts:
-	- A word2vec vector space mdoel that represents & converts tokenized sentences (from headlines) into vectors
-	- A random forest classifier that determines overall sentiment based on these vector embeddings
+	- A word2vec vector space model that represents & converts individual words (from headlines) into vectors
+	- A random forest classifier that determines overall sentiment based on a vector embedding for a headline. 
+		This vector embedding is created by averaging the vectors representing the headline's constituent words
 
 	ATTRIBUTES:
-		sentences_vector_space - a gensim.models.word2vec.Word2Vec object, representing the vector space model
+		words_vector_space - a gensim.models.word2vec.Word2Vec object, representing the vector space model
 		forest - an sklearn.assemble.RandomForestClassifier, representing the random forest classifier
 	"""
 
@@ -31,12 +33,15 @@ class SentimentAnalyzer():
 		"""
 
 		# Extract training data from CSV file
-		training_sentences = []
+		
 		training_sentiments = []
+		training_headlines = []
+		training_sentences = []
 		with open(csv_file_path, newline='') as file:
 			reader = csv.reader(file, delimiter=' ')
 			for row in reader:
 				training_sentiments.append(row[0])
+				training_headlines.append(row[1])
 
 				# Convert headline to 'sentence'
 				training_sentences += __headline_to_sentences(row[1])
@@ -44,22 +49,23 @@ class SentimentAnalyzer():
 
 		# Use word2vec to learn a vector space model for the sentences
 		try:
-			self.sentences_vector_space = Word2Vec.load("sentences_vector_space")
+			self.words_vector_space = Word2Vec.load("words_vector_space")
 
 		except: # Case where no existant model is found - Create new model here
 			
-			print("NO SENTENCES VECTOR SPACE MODEL FOUND. CREATING NEW MODEL.")
-			self.sentences_vector_space = word2vec.Word2Vec(
+			print("NO WORD2VEC VECTOR SPACE MODEL FOUND. CREATING NEW MODEL.")
+			self.words_vector_space = word2vec.Word2Vec(
 				sentences=training_sentences,
 				vector_size=100,
 				min_count=10,
 				workers=4
 			)
-			self.sentences_vector_space.init_sims(replace=True)
-			self.sentences_vector_space.save("sentences_vector_space") # Save model
+			self.words_vector_space.init_sims(replace=True)
+			self.words_vector_space.save("words_vector_space") # Save model
 
 		# Create and train random forest classifier on vectors & yvalues
 		self.forest = RandomForestClassifier(n_estimators=100)
+		training_data_vecs = [__average_feature_vec(headline) for headline in training_headlines]
 		self.forest = forest.fit(training_data_vecs, training_sentiments)
 
 
@@ -69,6 +75,8 @@ class SentimentAnalyzer():
 		Given a headline, convert it to a list of 'sentences' i.e. a list of lists word tokens,
 		each sublist represents a single sentence, and each string in it represents a word.
 		Essentially this is tokenization.
+
+		This is done to generate sentences on which to train the word2vec model
 
 		This method also "cleans" the sentence to prepare for sentiment analysis by:
 			- Removing non-letter chars
@@ -106,6 +114,36 @@ class SentimentAnalyzer():
 
 		return sentences
 
+
+	def __average_feature_vec(self, headline):
+		"""
+		Get the average feature vector for a headline.
+		Since our word2vec vector space model is able to convert individual words to vectors,
+		we can get the average vector of all of a headline's constituent words to get a single vector to represent a headline.
+
+		PARAMS:
+			headline - The headline as a string
+		"""
+
+		words = headline.split() # Tokenize headline to get array of words
+
+	    # Set containing all words in vector space model's vocabulary
+	    model_vocabulary = set(self.words_vector_space.index2word)
+
+	    # Initialize resultant feature vec
+	    result = np.zeros((100),dtype="float32")
+	    nwords = 0.
+
+	    # If word is in the vector space model's vocab, add it's vector to the result
+	    for word in words:
+	        if word in index2word_set: 
+	            nwords = nwords + 1.
+	            result = np.add(self.words_vector_space[word])
+
+	    # Divide the result by the number of words to get the average
+	    result = np.divide(result, nwords)
+	    return result
+
  
 	def est_sentiment(self, headlines):
 		"""
@@ -116,10 +154,7 @@ class SentimentAnalyzer():
 			headlines - A list of headline strings
 		""" 
 
-		# Convert headlines to 'sentences'
-		sentences = []
-		for headline in headlines:
-			sentences += __headline_to_sentences(headline)
+		headline_feature_vec = __average_feature_vec(headline)
 
-		result = self.forest.predict(features)
+		result = self.forest.predict(headline_feature_vec)
 		return result
