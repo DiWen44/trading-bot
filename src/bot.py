1784efb62bf3f19cae4fe602cdd6a2fc0c370b7f
Devin Sidhu <devinsidhu24@gmail.com>
2024-09-10 17:46:53 +0100

    - Since we need finnhub premium to get candle charts, and since getting   entire candles wasn't necessary for our purposes, deleted the   __get_candlestick_data() method.
    - Watchlist dataframe no longer keeps track of latest_price, and
      thus watchlist.csv no longer records it
    
    - __get_headlines() only returns headlines of articles where the company
      is mentioned IN THE HEADLINE ITSELF, rather than in the article body.
    This is to avoid collecting articles that only make a passing mention of
    the company, and therefore ensuring collected headlines are relevant to
    the company

--- src/bot.py
+++ src/bot.py
@@ -23,10 +23,7 @@ class Bot():
 
 		newsapi_key - The API key for newsAPI
 
-		watchlist - A pandas dataframe containing the names and ticker symbols of stocks we will be trading, as well as how much USD worth of that stock the bot is holding,
-		 			and the latest recorded price of that stock. Note that this last column does not exist to begin with in the starting watchlist.csv, 
-					but will be added to both dataframe and file when this class is instantiated for the first time
-					and will be connstantly updated with data from the finnhub websocket
+		watchlist - A pandas dataframe containing the names and ticker symbols of stocks we will be trading, as well as the quantity of stock the bot is holding (owned).
 
 		sentiment_analyzer - The sentiment analysis engine, of type SentimentAnalyzer
 
@@ -42,8 +39,6 @@ class Bot():
 
 		# Load watchlist from watchlist.csv into a dataframe
 		self.watchlist = pd.read_csv('watchlist.csv')
-		if 'latest_price' not in self.watchlist: # If latest price was not stored in the csv file 
-			self.watchlist['latest_price'] = ''
 
 		finnhub_key = os.getenv('FINNHUB_KEY')
 		self.finnhub_client = finnhub.Client(api_key=finnhub_key)
@@ -53,7 +48,7 @@ class Bot():
 												  on_message=self.__on_finnhub_websocket_message,
 												  on_error=self.__on_finnhub_websocket_error,
 												  on_close=self.__on_finnhub_websocket_close)
-
+		
 		self.newsapi_key = os.getenv('NEWSAPI_KEY')
 
 		self.sentiment_analyzer = sentimentAnalyzer.SentimentAnalyzer('training_data.csv')
@@ -65,7 +60,7 @@ class Bot():
 	def __del__(self):
 		"""
 		DESTRUCTOR METHOD
-		Saves the watchlist, with it's updated latest price data, back to the watchlist.csv file.
+		Saves the watchlist, with it's updated stock holding info, back to the watchlist.csv file.
 		Also saves updated cash quantity back to cash.txt.
 		"""
 		
@@ -91,15 +86,18 @@ class Bot():
 	def __on_finnhub_websocket_message(self, ws, message):
 		"""
 		To be passed to constructor for the finnhub WebSocketApp, when that class is instantiated in bot's constructor.
-		Called automatically when bot receives data from finnhub's websocket server.
-		Since in __on_websocket_open() we subscribed to stock prices, this will be called when finnhub sends a price update
+		Called automatically when bot receives new data from finnhub's websocket server.
+		Since in __on_websocket_open() we subscribed to stock prices, this will be called when finnhub sends a price update.
+
+		We want to execute our trading algorithm every time new data comes in from finnhub, so we will call trading_strategy here
 		""" 
 		
 		print(message)
 		msg_dict = ast.literal_eval(message)
 		symbol = msg_dict['data'][0]['s']
 		latest_price = msg_dict['data'][0]['p']
-		self.watchlist.loc[self.watchlist['symbol'] == symbol]['latest_price'] = latest_price # Update latest price 
+
+		self.__trading_strat(symbol, latest_price)
 
 	
 	def __on_finnhub_websocket_error(self, ws, error):
@@ -117,9 +115,9 @@ class Bot():
 		Called automatically when the websocket connection is closed
 		"""
 		print("FINNHUB WEBSOCKET CONNECTION CLOSED")
+ 
 
-
-	def __get_headlines(self, company, days):
+	def get_headlines(self, company, days):
 		"""
 		Gets news headlines from newsAPI that are pertinent to a given company.
 		Returned in the form of an array of strings, each string representing the headline of an article
@@ -133,56 +131,27 @@ class Bot():
 		start_date = (datetime.today() - timedelta(days=days)).strftime('%Y-%m-%d')
 
 		# Get a dictionary object of the news (newsAPI returns a JSON object)
-		newsJSON = requests.get(f"https://newsapi.org/v2/everything?q={company}&from={start_date}&sortBy=popularity&apiKey={self.newsapi_key}").json()
-		news = json.loads(newsJSON)
+		news = requests.get(f"https://newsapi.org/v2/everything?q={company}&searchin=title&language=en&from={start_date}&sortBy=popularity&apiKey={self.newsapi_key}").json()
 
-		headlines = [ (article['title'] + ". " + article['description']) for article in news['articles'] ] # NewsAPI data seperates headlines into title and description - concatenate these 2 together for our purposes
+		headlines = [ (article['title'] + ". " + article['description']) for article in news['articles'] if article['description'] is not None] # NewsAPI data seperates headlines into title and description - concatenate these 2 together for our purposes
 		return headlines
 
 
-	def __get_candlestick_data(self, symbol, days):
-		"""
-		Gets stock price candlestick data from finnhub for a given company.
-		The candles' interval will be in days, so each candlestick represents the stock over a day (rather than a week or month) 
-		Returns this data as a pandas dataframe of the finnhub API response
-
-		PARAMS:
-			symbol - Ticker symbol of the company to get data for
-			days - Time window (in days) in which to get data (e.g. days=3 will get timeseries data over the last 3 days)
-		"""
-
-		present_date = datetime.today() 
-		window_start_date = present_date - timedelta(days=days)
-
-		candles = self.finnhub_client.stock_candles(symbol, 'D', present_date.timestamp(), window_start_date.timestamp())
-		return pd.DataFrame(candles)
-
-
-	def __trading_strat(self, company, symbol, value_owned, latest_price):
+	def __trading_strat(self, symbol, latest_price):
 		"""
 		Actually executes trades based on collated data.
 		Essentially, this method defines the trading strategy with which we approach individual stocks
 
 		PARAMS:
-			company - name of the company whose stock is to be traded
-			symbol - the company's ticker symbol
-			value_owned - The USD worth of company's stock that the bot currently holds
-			latest_price - The latest price of the company's stock
+			symbol - the ticker symbol of the company whose stock to trade
+			latest_price - The latest recorded price of the company's stock
 		"""
 
-		# Get candlestick data & news over the last day
-		candles = self.__get_candlestick_data(symbol, 1)
-		headlines = self.__get_headlines(company, 1)
+		# Get news over the last day
+		company_name = self.watchlist.loc[self.watchlist['symbol'] == symbol]['name']
+		headlines = self.__get_headlines(company_name, 1)
 
 		sentiment = self.sentiment_analyzer.est_sentiment(headlines)
-
-
-	def trade(self):
-		"""
-		Executes the trading strategy over all stocks on the watchlist
-		"""
-
-		self.watchlist['value_owned'] = self.watchlist.apply(self.__trading_strat, axis=1)
 		
 
 
--- src/main.py
+++ src/main.py
@@ -2,4 +2,6 @@ import sentimentAnalyzer
 import bot
 
 bot = bot.Bot()
+
+print(bot.get_headlines('apple', 3))
 del bot # Explicitly call bot's destructor
